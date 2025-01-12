a36778ae9fd9b35699a9f90121b7dcee89f1437f
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-01-12 14:33:20 +0100

    use new version check

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <windows.h>
 #include "nya_commonhooklib.h"
+#include "fo2versioncheck.h"
 
 BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
 	switch( fdwReason ) {
 		case DLL_PROCESS_ATTACH: {
-			if (NyaHookLib::GetEntryPoint() != 0x24CEF7) {
-				MessageBoxA(nullptr, aFOUCVersionFail, "nya?!~", MB_ICONERROR);
-				exit(0);
-				return TRUE;
-			}
-
+			DoFlatOutVersionCheck(FO2Version::FOUC_GFWL);
 			NyaHookLib::PatchRelative(NyaHookLib::JMP, 0x55B969, 0x55BA58); // skip copyright screen
 			NyaHookLib::Patch<uint8_t>(0x55B29D, 0xEB); // skip intros
 		} break;
